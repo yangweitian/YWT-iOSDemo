3344bd74cd0b5b96f18d9618d96e6080dd46f77f
yangweitian <983083931@qq.com>
2016-01-29 10:57:43 +0800

    新建空白项目

---
+++ YWT/YWT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWT
+//
+//  Created by YangWeitian on 16/1/29.
+//  Copyright © 2016年 YangWeitian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWT/YWT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWT
+//
+//  Created by YangWeitian on 16/1/29.
+//  Copyright © 2016年 YangWeitian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
